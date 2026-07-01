4dbff20a5779bb1716beeed075dbe32f45ba47cc
Abo Bakr Bawazir <abobakr.om.zr@gmail.com>
2026-07-01 06:24:39 +0000

    Add cafe bill calculation functions

---
+++ week_5/main.py
@@ -0,0 +1,21 @@
+# main.py
+
+from utils import calculate_total, print_receipt
+
+def main():
+
+    print("===== CAFE BILL SYSTEM =====")
+
+    name = input("Enter customer name: ")
+
+    coffee = int(input("Enter coffee quantity: "))
+    tea = int(input("Enter tea quantity: "))
+    sandwich = int(input("Enter sandwich quantity: "))
+
+    total = calculate_total(coffee, tea, sandwich)
+
+    print_receipt(name, coffee, tea, sandwich, total)
+
+
+# Run program
+main()
